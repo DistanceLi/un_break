48c0ff9f904387558b44d75f34e5d2984054d478
wintjade <winterxuc@gmail.com>
2021-04-03 18:30:53 +0800

    update

---
+++ run_scripts.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+SCRIPT_NAME=`echo "${1}" | awk -F "." '{print $1}'`
+LOG="./${SCRIPT_NAME}.log"
+
+REPO_URL="https://github.com/tracefish/ds"
+REPO_BRANCH="sc"
+[ ! -d ~/ds ] && git clone -b "$REPO_BRANCH" $REPO_URL ~/ds
+
+cd ~/scripts
+
+logDir="../ds"
+# 格式化助力码
+autoHelp(){
+# $1 脚本文件
+# $2 助力码文件所在
+    sr_file=$1
+    sc_file=$2
+    sc_list=(`cat "$sc_file" | while read LINE; do echo $LINE; done | awk -F "】" '{print $2}'`)
+    f_shcode=""
+    IFS=$'\n'
+    JK_LIST=(`echo "$JD_COOKIE" | awk -F "&" '{for(i=1;i<=NF;i++) print $i}'`)
+    [ -z "$JK_LIST" ] && JK_LIST=(`echo "$JD_COOKIES" | awk -F "&" '{for(i=1;i<=NF;i++) print $i}'`)
+    if [ -n "$JK_LIST" ]; then
+        diff=$((${#JK_LIST[*]}-${#sc_list[*]}))
+        for e in `seq 1 diff`
+        do 
+            sc_list+=(${sc_list[0]})
+            unset sc_list[0]
+            sc_list=(${sc_list[*]})
+            f_shcode="$f_shcode""'""`echo ${sc_list[*]:0} | awk '{for(i=1;i<=NF;i++) {if(i==NF) printf $i;else printf $i"@"}}'`""',""\n"
+        done
+    fi
+    for e in `seq 1 ${#sc_list[*]}`
+    do 
+        sc_list+=(${sc_list[0]})
+        unset sc_list[0]
+        sc_list=(${sc_list[*]})
+        f_shcode="$f_shcode""'""`echo ${sc_list[*]:0} | awk '{for(i=1;i<=NF;i++) {if(i==NF) printf $i;else printf $i"@"}}'`""',""\n"
+    done
+    unset IFS
+    [ -n "$MY_SHARECODES" ] && f_shcode="$f_shcode""'$MY_SHARECODES',\n"
+    sed -i "s/let shareCodes = \[/let shareCodes = \[\n${f_shcode}/g" "./$sr_file"
+    sed -i "s/const inviteCodes = \[/const inviteCodes = \[\n${f_shcode}/g" "./$sr_file"
+    sed -i "s/let inviteCodes = \[/let inviteCodes = \[\n${f_shcode}/g" "./$sr_file"
+    # 修改种豆得豆
+    if [ "$1" = "jd_plantBean.js" ]; then
+        sed -i "s/let PlantBeanShareCodes = \[/let PlantBeanShareCodes = \[\n${f_shcode}/g" "./jdPlantBeanShareCodes.js"
+    fi
+    # 修改东东萌宠
+    if [ "$1" = "jd_pet.js" ]; then
+        sed -i "s/let PetShareCodes = \[/let PetShareCodes = \[\n${f_shcode}/g" "./jdPetShareCodes.js"
+    fi
+    # 修改东东农场
+    if [ "$1" = "jd_fruit.js" ]; then
+        sed -i "s/let FruitShareCodes = \[/let FruitShareCodes = \[\n${f_shcode}/g" "./jdFruitShareCodes.js"
+    fi
+    # 修改京喜工厂
+    if [ "$1" = "jd_dreamFactory.js" ]; then
+        sed -i "s/let shareCodes = \[/let shareCodes = \[\n${f_shcode}/g" "./jdDreamFactoryShareCodes.js"
+    fi
+    # 修改东东工厂
+    if [ "$1" = "jd_jdfactory.js" ]; then
+        sed -i "s/let shareCodes = \[/let shareCodes = \[\n${f_shcode}/g" "./jdFactoryShareCodes.js"
+    fi
+}
+
+if [ -n "$SYNCURL" ]; then
+    echo "下载脚本"
+    curl "$SYNCURL" > "./$1"
+    # 外链脚本替换
+    sed -i "s/indexOf('GITHUB')/indexOf('GOGOGOGO')/g" `ls -l |grep -v ^d|awk '{print $9}'`
+    sed -i 's/indexOf("GITHUB")/indexOf("GOGOGOGO")/g' `ls -l |grep -v ^d|awk '{print $9}'`
+fi
+[ ! -e "./$1" ] && echo "脚本不存在" && exit 0
+
+echo "替换助力码"
+[ -e "${logDir}/${SCRIPT_NAME}.log" ] && autoHelp "${1}" "${logDir}/${SCRIPT_NAME}.log"
+# 支持并行的cookie
+if [ -n "$JD_COOKIES" ]; then
+  echo "修改cookie"
+  sed -i 's/process.env.JD_COOKIE/process.env.JD_COOKIES/g' ./jdCookie.js
+fi
+
+echo "DECODE"
+encode_str=(`cat ./$1 | grep "window" | awk -F "window" '{print($1)}'| awk -F "var " '{print $(NF-1)}' | awk -F "=" '{print $1}' | sort -u`)
+if [ -n "$encode_str" ]; then
+    for ec in ${encode_str[*]}
+    do
+        sed -i "s/return $ec/if($ec.toLowerCase()==\"github\"){$ec=\"GOGOGOGO\"};return $ec/g" ./$1
+    done
+fi
+
+echo "开始运行"
+(node ./$1 | grep -Ev "pt_pin|pt_key") >&1 | tee ${LOG}
+
+# 收集助力码
+collectSharecode(){
+    echo "${1}：收集新助力码"
+    code=`sed -n '/'码】'.*/'p ${1}`
+    if [ -z "$code" ]; then
+        activity=`sed -n '/配置文件.*/'p "${LOG}" | awk -F "获取" '{print $2}' | awk -F "配置" '{print $1}'`
+        name=(`sed -n '/'【京东账号'.*/'p "${LOG}" | grep "开始" | awk -F "开始" '{print $2}' |sed 's/】/（/g'| awk -v ac="$activity" -F "*" '{print $1"）" ac "好友助力码】"}'`)
+        code=(`sed -n '/'您的好友助力码为'.*/'p ${1} | awk '{print $2}'`)
+        [ -z "$code" ] && code=(`sed -n '/'好友助力码'.*/'p ${1} | awk -F "：" '{print $2}'`)
+        [ -z "$code" ] && exit 0
+        for i in `seq 0 $((${#name[*]}-1))`
+        do 
+            echo "${name[i]}""${code[i]}" >> ${LOG}1
+        done
+    else
+        echo $code | awk '{for(i=1;i<=NF;i++)print $i}' > ${LOG}1
+    fi
+}
+
+collectSharecode ${LOG}
+cat ${LOG}1
+[ ! -e "${LOG}1" -o -z "$(cat ${LOG}1)" ] && echo "退出脚本" && exit 0
+echo "上传助力码文件"
+cd ~/ds
+echo "拉取最新源码"
+git config --global user.email "tracefish@qq.com"
+git config --global user.name "tracefish"
+git pull origin "$REPO_BRANCH:$REPO_BRANCH"
+
+echo "Resetting origin to: https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
+sudo git remote set-url origin "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY"
+
+echo "强制覆盖原文件"
+mv -v ~/scripts/${LOG}1 ${LOG}
+git add .
+git commit -m "update ${SCRIPT_NAME} `date +%Y%m%d%H%M%S`"
+
+echo "Pushing changings from tmp_upstream to origin"
+sudo git push origin "$REPO_BRANCH:$REPO_BRANCH" --force
